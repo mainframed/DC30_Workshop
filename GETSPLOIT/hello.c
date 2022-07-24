48a838800d19b4c2c49c68e6a16ac42e47770dc6
mainframed <mainframed767@gmail.com>
2022-07-24 10:49:20 -0700

    making hello.c fancy

--- GETSPLOIT/hello.c
+++ GETSPLOIT/hello.c
@@ -6,6 +6,29 @@ int main (int argc, char ** argv) {
    char buff[150];
    printf("Hi, D3CF0N attendee what is your handle?\n");
    gets(buff);
-   printf("Follow the white rabbit, %s", buff);
+   printf("Wake up, %s\nThe Matrix has you...", buff);
+   printf("Follow the white rabbit.\n\n", buff);
+   printf("            _________________\n"
+"           /            __   \\ \n"
+"           |           (__)  | \n"
+"           |                 | \n"
+"           | .-----.   .--.  | \n"
+"           | |     |  /    \\ | \n"
+"           | '-----'  \\    / | \n"
+"           |           |  |  | \n"
+"           | LI DC LI  |  |  | \n"
+"           | LI 30 LI  |  |  |Oo \n"
+"           | LI LI LI  |  |  |`Oo \n"
+"           | LI LI LI  |  |  |  Oo \n"
+"           |           |  |  |   Oo \n"
+"           | .------. /    \\ |   oO \n"
+"           | |      | \\    / |   Oo \n"
+"           | '------'  '-oO  |   oO \n"
+"           |          .---Oo |   Oo \n"
+"           |          ||  ||`Oo  oO \n"
+"           |          |'--'| | OoO \n"
+"           |          '----' | \n"
+"           \\_________________/ \n\n"
+"            Ring Ring %s\n\n", buff);
    return 0;
 };
